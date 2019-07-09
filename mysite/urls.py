f206b7a759abfffddf22f3f6e4b7b10d37d04b89
DeepakSingh9 <indeepak9@gmail.com>
2019-07-09 12:05:38 +0530

    'folders'

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,6 +26,3 @@ urlpatterns = [
     url(r'^',include('signup.urls')),
     url(r'^',include('resume.urls')),
 ]
-if settings.DEBUG is False:
-    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
